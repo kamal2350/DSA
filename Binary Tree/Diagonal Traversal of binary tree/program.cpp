564d2145c1baa2a9792b84f4e11e89d99cbc9a7f
kamal2350 <61842478+kamal2350@users.noreply.github.com>
2022-06-29 08:11:20 +0530

    Create program.cpp

---
+++ Binary Tree/Diagonal Traversal of binary tree/program.cpp
@@ -0,0 +1,36 @@
+// 1. use map to store node with particular diagonal value;
+// 2. create helper function and pass the reference of map;
+// 3. *** use diogonal value same for right node and diagonal+1 value for nodes.
+
+
+void helper(Node *root, int diagonal,unordered_map<int,vector<int>> &map){
+    if(root==NULL) return ;
+    
+    map[diagonal].push_back(root->data);
+    helper(root->left,diagonal+1,map);
+    helper(root->right,diagonal,map);
+    return;
+}
+vector<int> diagonal(Node *root)
+{
+   // your code here
+   unordered_map<int,vector<int>> map;
+   vector<int> vec;
+   if(root==NULL){
+       return vec;
+   }
+   if(root->left==NULL && root->right==NULL){
+       vec.push_back(root->data);
+       return vec;
+   }
+    helper(root,0,map);
+   
+   for(int i=0;i<map.size();i++){
+       for(auto m:map[i]){
+           vec.push_back(m);
+       }
+   }
+   return vec;
+   
+   
+ 
